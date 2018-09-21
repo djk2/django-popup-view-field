42ee683d97ff3a013c8aba8e0795fbe7cfa68936
Grzegorz Tężycki <grzegorz.tezycki@mga.com.pl>
2018-09-21 22:22:10 +0200

    V1.0.0 - Work in progres

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from .views import Index
+from .views import Index, IndexBootstrap3, IndexBootstrap4
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', Index.as_view(), name="index"),
+    url(r'^bootstrap3/$', IndexBootstrap3.as_view(), name="index_b3"),
+    url(r'^bootstrap4/$', IndexBootstrap4.as_view(), name="index_b4"),
 
     # django-popup-view-field
     url(r'^django_popup_view_field/', include('django_popup_view_field.urls')),
